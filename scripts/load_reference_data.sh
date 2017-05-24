44a7837cabd74b3b8cf2a7b94b3629d1a0256449
msneddon <mwsneddon@lbl.gov>
2017-05-24 09:47:52 -0700

    all ref data now generated

--- scripts/load_reference_data.sh
+++ scripts/load_reference_data.sh
@@ -30,45 +30,45 @@ if [ $fail -eq 1 ] ; then
 fi
 
 date
-# get_file 'cat.tar'
-# check_exists 'cat_genome'
-# if [ $fail -eq 1 ] ; then
-#     exit 1
-# fi
+get_file 'cat.tar'
+check_exists 'cat_genome'
+if [ $fail -eq 1 ] ; then
+    exit 1
+fi
 
-# date
-# get_file 'dog.tar'
-# check_exists 'dog_genome'
-# if [ $fail -eq 1 ] ; then
-#     exit 1
-# fi
+date
+get_file 'dog.tar'
+check_exists 'dog_genome'
+if [ $fail -eq 1 ] ; then
+    exit 1
+fi
 
-# date
-# get_file 'hg19.tar'
-# check_exists 'hg194'
-# if [ $fail -eq 1 ] ; then
-#     exit 1
-# fi
+date
+get_file 'hg19.tar'
+check_exists 'hg194'
+if [ $fail -eq 1 ] ; then
+    exit 1
+fi
 
-# date
-# get_file 'mouse.tar'
-# check_exists 'mouse_genome'
-# if [ $fail -eq 1 ] ; then
-#     exit 1
-# fi
+date
+get_file 'mouse.tar'
+check_exists 'mouse_genome'
+if [ $fail -eq 1 ] ; then
+    exit 1
+fi
 
-# date
-# get_file 'mousecatdoghuman.tar'
-# check_exists 'mousecatdoghuman'
-# if [ $fail -eq 1 ] ; then
-#     exit 1
-# fi
+date
+get_file 'mousecatdoghuman.tar'
+check_exists 'mousecatdoghuman'
+if [ $fail -eq 1 ] ; then
+    exit 1
+fi
 
-# date
+date
 
-#echo $fail
-#if [ $fail -eq 0 ] ; then
-#    # touching this file indicates success
-#    echo "Success.  Writing __READY__ file."
-#    touch /data/__READY__
-#fi
+echo $fail
+if [ $fail -eq 0 ] ; then
+   # touching this file indicates success
+   echo "Success.  Writing __READY__ file."
+   touch /data/__READY__
+fi
