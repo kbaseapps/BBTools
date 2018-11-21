6dfcf6526bf3efda4b148c82681d37099a3ea9db
Bill Riehl <wjriehl@lbl.gov>
2018-11-21 15:07:47 -0800

    flesh out memory estimator tests

--- lib/BBTools/utils/MemEstimatorRunner.py
+++ lib/BBTools/utils/MemEstimatorRunner.py
@@ -1,5 +1,6 @@
 import re
 import subprocess
+import os
 
 REFORMAT_PATH = "/kb/module/bbmap/reformat.sh"
 
@@ -13,6 +14,13 @@ class MemEstimatorRunner(object):
     def check_params(self):
         if not self.file:
             raise ValueError('Parameter "file" must be present!')
+        if not os.path.exists(self.file) and not os.path.isfile(self.file):
+            raise ValueError('The file {} does not seem to exist, or is a directory'.format(self.file))
+        if self.file2:
+            if not os.path.exists(self.file2) and not os.path.isfile(self.file2):
+                raise ValueError('The file {} does not seem to exist, or is a directory'.format(self.file2))
+        if self.file == self.file2:
+            raise ValueError("If two files are present, they must be different.")
 
     def run(self):
         # use reformat.sh from bbtools to calculate unique 31mers
@@ -24,8 +32,12 @@ class MemEstimatorRunner(object):
         process = subprocess.Popen(cmd, stderr=subprocess.PIPE)
         output = str(process.communicate()[1])
 
-        m = re.search(r'Unique 31-mers:.*?(\d+)', output)
-        uniq_kmers = m.group(1)
+        print(output)
+        m = re.search(r'Unique 31-mers:\s*(\d+)', output)
+        if m is not None:
+            uniq_kmers = m.group(1)
+        else:
+            raise ValueError("Count of unique 31-mers not found. Run result:\n{}".format(output))
 
         # This is the formula to estimate ram required (in gigs)
         # Gb ~ 6e-8(N) where N is unique 36mers
--- test/mem_estimator_test.py
+++ test/mem_estimator_test.py
@@ -1,12 +1,19 @@
 import unittest
 from BBTools.BBToolsImpl import BBTools
+from BBTools.BBToolsServer import MethodContext
+from BBTools.authclient import KBaseAuth as _KBaseAuth
 from BBTools.utils.MemEstimatorRunner import MemEstimatorRunner
+import os
+try:
+    from ConfigParser import ConfigParser  # py2
+except:
+    from configparser import ConfigParser  # py3
 
 class MemEstimatorTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        token = environ.get('KB_AUTH_TOKEN', None)
-        config_file = environ.get('KB_DEPLOYMENT_CONFIG', None)
+        token = os.environ.get('KB_AUTH_TOKEN', None)
+        config_file = os.environ.get('KB_DEPLOYMENT_CONFIG', None)
         cls.cfg = {}
         config = ConfigParser()
         config.read(config_file)
@@ -27,8 +34,6 @@ class MemEstimatorTest(unittest.TestCase):
                              'method_params': []
                              }],
                         'authenticated': 1})
-        cls.wsURL = cls.cfg['workspace-url']
-        cls.ws = workspaceService(cls.wsURL)
         cls.serviceImpl = BBTools(cls.cfg)
         cls.scratch = cls.cfg['scratch']
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
@@ -43,16 +48,72 @@ class MemEstimatorTest(unittest.TestCase):
         self.assertGreater(estimate, 0)
 
     def test_mem_estimator_ok_pair(self):
-        in_fie = "data/interleaved.fastq"
         params = {
-            "file": in_file,
-            "file2": in_file
+            "file": "data/small.forward.fq",
+            "file2": "data/small.reverse.fq"
         }
         runner = MemEstimatorRunner(params)
         estimate = runner.run()
         self.assertGreater(estimate, 0)
 
-    def test_mem_estimator_missing_input(self):
+    def test_mem_estimator_pair_dups(self):
+        in_file = "data/interleaved.fastq"
+        params = {
+            "file": in_file,
+            "file2": in_file
+        }
+        with self.assertRaises(ValueError) as e:
+            MemEstimatorRunner(params)
+        self.assertIn("If two files are present, they must be different.", str(e.exception))
+
+    def test_mem_estimator_bad_file(self):
+        in_file = "data/bad_reads.txt"
+        params = {
+            "file": in_file
+        }
+        with self.assertRaises(ValueError) as e:
+            MemEstimatorRunner(params).run()
+        self.assertIn("Count of unique 31-mers not found.", str(e.exception))
+        self.assertNotIn("31-mers processed", str(e.exception))
+        self.assertNotIn("Correct 31-mers", str(e.exception))
+        self.assertNotIn("Unique 31-mers", str(e.exception))
+
+    def getImpl(self):
+        return self.__class__.serviceImpl
+
+    def getContext(self):
+        return self.__class__.ctx
+
+    def test_mem_estimator_e2e(self):
+        impl = self.getImpl()
+        ctx = self.getContext()
+        in_file = "data/interleaved.fastq"
+        params = {
+            "file": in_file
+        }
+        res = impl.run_mem_estimator(ctx, params)[0]
+        self.assertIn("estimate", res)
+        self.assertGreater(res["estimate"], 0)
+
+        params = {
+            "file": "data/small.forward.fq",
+            "file2": "data/small.reverse.fq"
+        }
+        res = impl.run_mem_estimator(ctx, params)[0]
+        self.assertIn("estimate", res)
+        self.assertGreater(res["estimate"], 0)
+
+    def test_bad_inputs(self):
+        impl = self.getImpl()
+        ctx = self.getContext()
         with self.assertRaises(ValueError) as e:
             MemEstimatorRunner({})
         self.assertIn('Parameter "file" must be present!', str(e.exception))
+
+        with self.assertRaises(ValueError) as e:
+            impl.run_mem_estimator(ctx, {"file": "not_real"})
+        self.assertIn("The file not_real does not seem to exist, or is a directory", str(e.exception))
+
+        with self.assertRaises(ValueError) as e:
+            impl.run_mem_estimator(ctx, {"file": "data/interleaved.fastq", "file2": "not_real2"})
+        self.assertIn("The file not_real2 does not seem to exist, or is a directory", str(e.exception))
